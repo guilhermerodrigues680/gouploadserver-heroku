681db60d62702b68b4f2c5c5f0e783b99be4ff22
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-04-20 09:41:49 -0300

    Add SPA com Vue.js para testes

---
+++ test/spa/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Not Found - This page does not exist :(</h1>
+    <p><router-link to="/">Go Home</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
